08c4e369da405b5d234db6018a8da9346287bf1d
BrandonK <brandon@kharazmi.com>
2020-10-09 21:44:10 -0400

    saves xml file to project folder
    aaaaddddsdsdsa

--- src/controller/upload.js
+++ src/controller/upload.js
@@ -1,10 +1,12 @@
 const { getArticleId } = require('../vendor/pubmed');
 const XMLWriter = require('xml-writer');
 const xmlReader = require('xml-reader');
+const fs = require('fs');
+const fileLocation = require('path');
 
  async function handler(req, res) {
     try {
-        console.log("Recieved a request to process a file");
+        console.log("Recieved a request to process a file.");
         if (!req.files) {
             console.log("No files uploaded");
             res.json({
@@ -17,7 +19,13 @@ const xmlReader = require('xml-reader');
             const titles = await extractArticleTitles(file.data.toString());
             const promises = titles.map(getArticleId);
             const results = await Promise.all(promises);
+            const xmlresults = writeXml(results);
+            console.log(xmlresults);
             res.send("Done Processing: " + JSON.stringify(results));
+            fs.writeFile('group_unknown_result.xml',xmlresults, function(err){
+                if (err) return console.log(err);
+                else return console.log('File written successfully!');
+            })
         }
     } catch(error) {
         console.log(error);
@@ -41,5 +49,10 @@ async function extractArticleTitles(xml) {
         reader.parse(xml);
     });
 }
+
+function writeXml(array) {
+    var body = array.map(item => `\n <PubmedArticle>\n      <PMID>${item.id}</PMID>\n      <ArticleTitle>${item.title}</ArticleTitle>\n </PubmedArticle>`).join('');
+    return `<PubmedArticleSet>${body}\n</PubmedArticleSet>`
+  }
 
 module.exports = handler;
